6d1463ea27fd52b5d3aaf2aead82a6324bbaee69
Suyashendra <suyashendrarathore9039@gmail.com>
2023-05-06 18:42:33 +0530

    9th commit

---
+++ SQL Joins Use Case.sql
@@ -0,0 +1,22 @@
+-- Joins Use Case 
+
+-- Use Case 1
+-- Find highest paid employee beside Michael Scott 
+
+select employeedemographics.employeeid, firstname, lastname, salary
+from employeedemographics 
+inner join employeesalary 
+on employeedemographics.employeeid = employeesalary.employeeid
+where firstname <> 'Michael'
+order by Salary desc
+limit 1;
+
+-- Use Case 2
+-- Calculate avgerage salary of Salesman
+
+select jobtitle, round(avg(salary)) as "Avg Salary"
+from employeedemographics 
+inner join employeesalary
+on employeedemographics.employeeid = employeesalary.employeeid
+where jobtitle = 'Salesman'
+group by jobtitle;
